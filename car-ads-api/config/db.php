f4bf3b2ff434e383e0290b8f695c22f05b9fc97e
1t1sCooL <137305666+1t1sCooL@users.noreply.github.com>
2025-06-07 15:31:17 +0300

    first commit

---
+++ car-ads-api/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;dbname=car_ads',
+    'username' => 'carApi',
+    'password' => 'carApi',
+    'charset' => 'utf8',
+];
